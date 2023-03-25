bdb2b9bf823a77e7fc173f7960df4bb259ab574d
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-03-25 10:28:29 +0545

    Add files via upload

---
+++ 1_introduction.py
@@ -0,0 +1,16 @@
+# 1991 - Guido von Rossum
+# Python got its name from a famous show Monty Python.
+# Py is a general purpose programming langauge that supports OOP sequence.
+# py is interpreted and high level language
+#  it is simple and easy to learn
+# It is platfrom independent programming langauge
+# It is open source
+# provides very big library support. Eg-NumPy, TensorFlow
+# Possible to interpret other programming language within python
+
+# Why python is used?
+# In data visualization, creating plot, graphical representations
+# helps in data Analysis to understand raw data
+# used in AI and ML to simulate human behavior
+# used to handle databases
+# used to create web app, and etc
