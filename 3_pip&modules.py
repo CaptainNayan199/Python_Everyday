073cee8c2d0a190fea382f6a9b01267afd5fd1ae
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-03-25 10:33:16 +0545

    Add files via upload
    Basic about Pip and Modules.

---
+++ 3_pip&modules.py
@@ -0,0 +1,15 @@
+# modules - it is the moethod of using other developers already developed code. Modules are well tested,
+# less error, used by many people, it will sabe time and efforts and can also be used in many other big projects.
+# Modules are like a code library which can be used to borrow a code written by somebody else.
+
+# Modules are of two types in pythons: 1) Built-In modules and 2)External modules
+# Built in modules are those types of modules which are already built in python itself, it comes when we 
+# download and install python.
+# External modules are those types of modules which are not embedded in python but are built by other developers and
+# other person or developers can used them by importing those modules using pip commands.
+
+# PIP commands : Pip is a package manager which stands for prefered installer program. This command is used to import
+# or install various python modules.
+
+# for example:
+# pip install pandas - this will install a module named pandas
