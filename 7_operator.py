74639b1a2b33427adcb7881c7bf624c2cae5fd5d
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-03-25 10:56:32 +0545

    Add files via upload
    Operator in Python

---
+++ 7_operator.py
@@ -0,0 +1,47 @@
+# there are many operator in python as like in other programming languages.
+# Some of it includes : + , - , * , / , // , % , **, etc.
+
+# Lets see some operator in brief: 
+
+# Adition operator (+)
+a = 6
+b =12
+sum = a+b
+print(sum)
+
+# Subtraction Operator (-)
+a = 12
+b = 4
+difference = a-b
+print(difference)
+
+# Multiply operator (*)
+a = 3
+b = 4
+product = a*b
+print(product)
+
+# Division operator (/)
+a = 6
+b = 2
+division = a/b
+print(division)
+
+# Floor division operator (//)
+a = 15
+b = 4
+floor = a//b
+print(floor)
+
+# modulo/remainder operator (%)
+a=7
+b=3
+mod= a%b
+print(mod)
+
+# Exponential operator (**)
+a = 2
+b = 4
+exp = (a**b)
+print(exp)
+
