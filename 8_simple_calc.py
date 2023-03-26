f0e9d42f08f82ee27cd6c9f0c6ea905c485352b4
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-03-26 20:00:09 +0545

    Wrote some comments

--- 8_simple_calc.py
+++ 8_simple_calc.py
@@ -1,4 +1,8 @@
 #A simple calculator project, you can this as your first python project.
+#Remember if you dont know about concatenations and all thos stufss,
+#dont worry i will make you know it in the coming future classes.
+#for today you can see how this code works, and always remember to
+# play with the code, try writing code in another way, try, and try
 
 print("Hello!")
 print("This is a simple and a basic calculator.")
