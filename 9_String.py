23aa81db6c50831a50c7fb63a6052ef64cd85659
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-03-27 21:18:14 +0545

    File Updated

--- 9_String.py
+++ 9_String.py
@@ -2,6 +2,11 @@
 #It is like ans array of characters.
 # enclosesed inside of double quote
 
+name = "Nayan"
+friend = 'Santosh'
+friend2 = 'Roshan'
+apple = "He said \"I want to eat apple today\""
+
 
 
 
