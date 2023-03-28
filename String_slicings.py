8c332d0bf7f912f0140591c731c4b148d4e34458
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-03-28 21:01:50 +0545

    #String slicings
    name ="Nayan"
    print(name[0:5])
    print(name[1:4])
    print(name[2:3])
    print(name[-3:-2])
    print(name[:-1]) # Python interpretor will add 0 automatically
    print(name[2:-1])
    
    #Findings length of that strings
    # length = len(name)
    # print("Name is "+str(length)+" character long")

---
+++ String_slicings.py
@@ -0,0 +1,15 @@
+#String slicings
+
+name ="Nayan"
+print(name[0:5])
+print(name[1:4])
+print(name[2:3])
+print(name[-3:-2])
+print(name[:-1]) # Python interpretor will add 0 automatically
+print(name[2:-1])
+
+#Findings length of that strings
+# length = len(name)
+# print("Name is "+str(length)+" character long")
+
+
