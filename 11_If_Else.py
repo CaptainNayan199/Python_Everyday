a86a4b4958bc5b6903ba58719039e52fdde7f4eb
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-03-29 21:38:29 +0545

    File updated

--- 11_If_Else.py
+++ 11_If_Else.py
@@ -1,7 +1,7 @@
 # condition in python: Condition statement in python are same as in other programming languages.
 # few conditonal keywords are different in strucutres or else all are same.
 
-#if elif else
+#if elif else are main 3 conditional controlling structuring statement in python
 
 
 #conditional operator in python
