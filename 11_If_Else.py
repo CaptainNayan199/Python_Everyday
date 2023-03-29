2d34f2b57b421aafa00287d3434e3216715cc7cf
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-03-29 21:39:54 +0545

    File updated

--- 11_If_Else.py
+++ 11_If_Else.py
@@ -8,7 +8,7 @@
 #conditional operator in python
 # <, >, >, <=, ==, !=
 
-# a = int(input("Enter a number : "))
+# a = (input("Enter a number : "))
 # if a%2==0 and a!=0:
 #     print("Even")
 # elif a%2==1:
