daccfc267e10ca35266fbffcf91bd6a38b3b9b43
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-03-29 21:38:00 +0545

    File updated

--- 11_If_Else.py
+++ 11_If_Else.py
@@ -1,4 +1,6 @@
-#condition in python
+# condition in python: Condition statement in python are same as in other programming languages.
+# few conditonal keywords are different in strucutres or else all are same.
+
 #if elif else
 
 
