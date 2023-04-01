d472eb1487b5d0791d7ee100feac651e66630510
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-01 12:19:53 +0545

    File Updated.

--- 12_If_Else_Ex.py
+++ 12_If_Else_Ex.py
@@ -9,7 +9,7 @@ import time
 hours = int(time.strftime("%I"))  # taking current time (in hour format, and formating it to int)
 ampm = time.strftime("%p") # taking sm/pm information
 
-name = input("Enter your name : ")
+name = input("Enter your name : ") #taking user input
 
 if (hours >= 4 and hours <= 12) and ampm == 'AM': # using if conditions
     print("Hello ", name.capitalize(), "Good Morning.......")
