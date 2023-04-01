955bca4be5350abbab929da74fa92385fd56e237
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-01 12:12:54 +0545

    File Updated.

--- 12_If_Else_Ex.py
+++ 12_If_Else_Ex.py
@@ -8,3 +8,4 @@ name = input("Enter your name : ")
 if (hours >= 4 and hours <= 12) and ampm == 'AM':
     print("Hello ", name.capitalize(), "Good Morning.......")
 elif (hours <=8) and ampm == 'PM':
+    print("Hello ", name.capitalize(), "Good Afternoon.......")
