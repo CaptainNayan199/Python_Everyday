594f61e14d1f9f249ec6033ed6a5dfb2119dce11
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-02 20:57:19 +0530

    File updated

--- 14_Match_case_Ex.py
+++ 14_Match_case_Ex.py
@@ -1,3 +1,4 @@
 # in this simple exercise we will be using match case logic to determine certain day as per user input
 # first of all we will take  integer user input and then determine on the basis of it
 
+week = int(input("Please enter a number between 1 to 7 : "))
