9a629950a59cec52a1c72e92d9b726f170af711c
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-02 20:46:00 +0530

    file updated

--- 13_Match_case.py
+++ 13_Match_case.py
@@ -2,4 +2,4 @@
 # first and foremost there is no switch case in python as like in other programming languages
 #Match case is a new feature added in python programming language. It was added after 3.10 version.
 #Match case is like switch case in other programming languages like C, Java, etc.
-#Match case is generally used for 
+#Match case is generally used for comparing values with the patterns
