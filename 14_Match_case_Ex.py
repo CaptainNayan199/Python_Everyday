cb616c5ee3f40f9f6a20d565d163dd1d05f3f01a
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-02 20:56:57 +0530

    File updated

--- 14_Match_case_Ex.py
+++ 14_Match_case_Ex.py
@@ -1,2 +1,3 @@
 # in this simple exercise we will be using match case logic to determine certain day as per user input
 # first of all we will take  integer user input and then determine on the basis of it
+
