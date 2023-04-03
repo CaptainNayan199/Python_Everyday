325075506fbe50eb234b48c578dbbd15390d4522
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-03 21:17:59 +0530

    File Updated

--- 15_For_Loops.py
+++ 15_For_Loops.py
@@ -54,5 +54,6 @@ for i in range (1, 12, 3):
     
 # the first parameter signifies the starting values of the range
 # the second parameter signifies the last/ending value of the range which is (n-1) or in our case(12-1)
+# the third parameter signifies the jumping value
     
 
