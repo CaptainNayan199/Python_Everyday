73f018d2671e5313858e52f50a7431263269f751
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-03 21:24:17 +0530

    File pdated

--- 15_For_Loops.py
+++ 15_For_Loops.py
@@ -7,20 +7,19 @@
 #Let's print numbers using For loop
 #We have to use range to print numbers in python
 
-# num = 10
-# for i in range (num): # i is used as certain variable that is storing the values from 0 to provided number.
-    # print(i) # the number from 0 to num-1 is printed
-# but what if we want to print starting from 1 ?
+num = 10
+for i in range (num): # i is used as certain variable that is storing the values from 0 to provided number.
+    print(i) # the number from 0 to num-1 is printed
+#but what if we want to print starting from 1 ?
 #just change inside print
 
-    # print(i+1) #starts printing from 1
+    print(i+1) #starts printing from 1
 
 # Likewise multiplication table can be also printed using for and range 
 
-# number = int(input("Which number table do you want ? "))
-
-# for i in range (0, 10):
-#     print(number, " X " , (i+1), " = ", number*(i+1), "\n"  )
+number = int(input("Which number table do you want ? "))
+for i in range (0, 10):
+    print(number, " X " , (i+1), " = ", number*(i+1), "\n"  )
     
 
 # Similaryl we can print charaters of a particular string using for loop as well
