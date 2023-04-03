dc2e8c2e02c67d8d5e947887b5d59331530c3351
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-03 20:54:40 +0530

    File Updated

--- 15_For_Loops.py
+++ 15_For_Loops.py
@@ -1,3 +1,5 @@
 #Loops are used for printing some sequence of codes for some n number of times.
 # They are used to execute some lines of codes for multiple times
 #For loop can iterate over a sequence of iterable objects in python
+
+#Numbers and strings/characters cn be printed in different way using for loops in python
