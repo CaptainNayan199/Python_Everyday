6def8131a89e8314d2dd536971e336f44d2bc3eb
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-03 20:59:45 +0530

    File Updated

--- 15_For_Loops.py
+++ 15_For_Loops.py
@@ -14,3 +14,5 @@ for i in range (num): # i is used as certain variable that is storing the values
 #just change inside print
 
     print(i+1) #starts printing from 1
+
+# Likewise multiplication table can be also printed using for and range 
