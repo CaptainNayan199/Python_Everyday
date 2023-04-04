ac4b43fd46a9ce77909a3af6dcadac079f06e0fe
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-04 20:43:35 +0530

    FIle Updated

--- 16_While_Loops.py
+++ 16_While_Loops.py
@@ -1,3 +1,4 @@
 # while loop iss another type of loop that is used to execute certain code for multiple times
 i = int(input("A number please : "))
 while (i<10):
+    i = int(input("A number please : "))
