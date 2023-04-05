01c283bbc5a9e93ca1c3b966a5cd6a60383ef218
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-05 22:05:22 +0530

    Modified Files

--- 17_Break_Continue.py
+++ 17_Break_Continue.py
@@ -1,1 +1,1 @@
-# Lopps in python run or work by each iteration and sometimes it can even rin for infinite times, and hence to 
+# Lopps in python run or work by each iteration and sometimes it can even rin for infinite times, and hence to stop that condition we should use break statement
