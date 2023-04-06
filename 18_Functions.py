af93f2f905590d3962c9020283ff55e824ab7a55
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-06 21:42:17 +0530

    FIle Updated

--- 18_Functions.py
+++ 18_Functions.py
@@ -1,6 +1,6 @@
 #functions in python are used to perform specific task whenever it is called
 
-def f_name(): # function in python is defined using keyword def, after def function name is provided, remember there is no use of curly braces and semicolons so remember to add colons a
+def f_name(): # function in python is defined using keyword def, after def function name is provided with parenthesis, remember there is no use of curly braces and semicolons so remember to add colons a
     # Some block of lines
     # Some block of lines
     print("Hello")
