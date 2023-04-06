c2bb197cdebe9a06d49e5b259c9cd686bee4f186
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-06 21:40:44 +0530

    FIle Updated

--- 18_Functions.py
+++ 18_Functions.py
@@ -1,6 +1,6 @@
 #functions in python are used to perform specific task whenever it is called
 
-def f_name(): # function in python is defined using keyword def, after def 
+def f_name(): # function in python is defined using keyword def, after def function name is provided
     # Some block of lines
     # Some block of lines
     print("Hello")
