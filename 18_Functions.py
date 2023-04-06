c62a687d001d581642bd70992953ca6b29de2fef
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-06 22:03:42 +0530

    File Updated

--- 18_Functions.py
+++ 18_Functions.py
@@ -21,3 +21,4 @@ empty_function()
 
 # 1. Built in functions - they are in build functions and are not required to invoke or create them. Eg : tuple(), hash(), set(), etc
 # 2. User defined functions - they are those functions that are built by users/developers as per their need
+
