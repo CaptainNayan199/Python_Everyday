1bda616667e8dff913d5e3d35b91f3249ed9d39d
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-06 22:01:02 +0530

    File Updated

--- 18_Functions.py
+++ 18_Functions.py
@@ -20,3 +20,4 @@ empty_function()
 # There are 2 types of function in python programming languages
 
 # 1. Built in functions - they are in build functions and are not required to invoke or create them. Eg : tuple(), hash(), set(), etc
+# 2. User defined functions - 
