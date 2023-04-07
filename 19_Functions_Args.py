0d8abf68567cef8e74028bf2a8fa11b4297d7605
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-07 21:45:22 +0530

    Files Updated

--- 19_Functions_Args.py
+++ 19_Functions_Args.py
@@ -1,1 +1,2 @@
 # Functions in python can be either with arguments or without arguments (parameters too; btw parameters and arguments are different things)
+# Functional arguments are placed inside a small brakcets
