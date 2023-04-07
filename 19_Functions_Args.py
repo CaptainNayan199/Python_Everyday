6931aa008c100bab148ab7fce6da3e10ffed0e3b
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-07 21:46:36 +0530

    Files Updated

--- 19_Functions_Args.py
+++ 19_Functions_Args.py
@@ -1,2 +1,3 @@
 # Functions in python can be either with arguments or without arguments (parameters too; btw parameters and arguments are different things)
 # Functional arguments are placed inside a small brakcets or ps: in python it can be given in form of sets, dictionaries too. (dont worry abt this things, we will see them in brief in upcoming sessions)
+#  Lets see the different types of arguments in python programming
