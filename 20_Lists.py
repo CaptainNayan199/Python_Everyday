6e56fc860c823496108ce53d4f05a54241823ffa
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-09 23:18:14 +0545

    Files Updated

--- 20_Lists.py
+++ 20_Lists.py
@@ -1,3 +1,4 @@
 # Lists in python are one of the most important data types 
 # They are used to store multiple values
 # They fall under the category of sequential data types as we have discussed in brief about all data types in past session
+# Lists are nothing but just an order collection of data where each elements are seperated by commas and are enclosed within a square brakcets[]
