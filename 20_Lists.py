bca8249be2c300d8039bd97d2d7b1637123371c7
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-09 23:19:22 +0545

    Files Updated

--- 20_Lists.py
+++ 20_Lists.py
@@ -3,3 +3,4 @@
 # They fall under the category of sequential data types as we have discussed in brief about all data types in past session
 # Lists are nothing but just an order collection of data where each elements are seperated by commas and are enclosed within a square brakcets[]
 # PS : Lists are mutable - i.e they can be changed later, are changeable and eidtableFiles Updated.
+# There
