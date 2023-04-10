669b3523f7a533ef970336938670e6bc278b5ea2
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-10 21:30:02 +0545

    Files Modified

--- 21_List_methods.py
+++ 21_List_methods.py
@@ -20,4 +20,4 @@ list3 = [1,3,45,6,2,56]
 print(list3,index(2)) #gives the indexing of the element 2 which is non other than 4
 
 list3 = [1,3,5,6,7,8,1,1,12]
-print(list3.count(1)) #
+print(list3.count(1)) #counts the repeatation of the certain elements.
