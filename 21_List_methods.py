63ff5eac3abc24c0e42bd3f797ed8a39cec7297d
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-10 21:22:59 +0545

    Files Modified

--- 21_List_methods.py
+++ 21_List_methods.py
@@ -1,2 +1,6 @@
 # Lists methods - here in this session we will be looking at some of the important lists methods that are very useful in manipulating certain lists
 # They are very important lists methods as we have to perform various operations on lists in python
+
+list1 = [1,2,3,4,5,6]
+print(list1)
+list1.append(7)
