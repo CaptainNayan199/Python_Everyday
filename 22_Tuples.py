b42c43db89b9ff5d186e90afe43096f9f34d17bd
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-11 20:20:19 +0545

    Files Modified

--- 22_Tuples.py
+++ 22_Tuples.py
@@ -45,4 +45,6 @@ print(newtup[30]) # this will throw error as value at index 30 is not there in t
 
 print(newtup[-2]) #this will give output = len(newtup) - 2 = 7, so it will give the value at 7 position  which is Pathak
 
+# checking if particular value is in the given list or not
+
 
