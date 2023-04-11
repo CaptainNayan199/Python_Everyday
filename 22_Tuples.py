11f6d820becaab518bb30b44603877cc4f0c6319
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-11 19:57:38 +0545

    Files Modified

--- 22_Tuples.py
+++ 22_Tuples.py
@@ -2,4 +2,5 @@
 # It is equally important as lists, and dictionaries(we'll talk abt it in future session).
 # They are order collection of data with each elements seperated by commas, and enclosed within parenthesis.
 # PS : Lists are enclosed inside a square brakcets, and TUples are enclosed inside a small brakets.
-# 
+
+# PS : Tuples are immutable data types in python.
