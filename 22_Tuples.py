7c6b8dd58b982af3c71f09fc853168b7f4aaf90f
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-11 19:57:14 +0545

    Files Modified

--- 22_Tuples.py
+++ 22_Tuples.py
@@ -2,3 +2,4 @@
 # It is equally important as lists, and dictionaries(we'll talk abt it in future session).
 # They are order collection of data with each elements seperated by commas, and enclosed within parenthesis.
 # PS : Lists are enclosed inside a square brakcets, and TUples are enclosed inside a small brakets.
+# 
