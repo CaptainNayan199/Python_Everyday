f928eef39a332406b76507e8ebd93bdd86ab3e67
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-11 19:58:34 +0545

    Files Modified

--- 22_Tuples.py
+++ 22_Tuples.py
@@ -4,4 +4,5 @@
 # PS : Lists are enclosed inside a square brakcets, and TUples are enclosed inside a small brakets.
 
 # PS : Tuples are immutable data types in python.
-# That means once they are created, they cannot be changed or modified further unline lists which are mutable
+# That means once they are created, they cannot be changed or modified further unline lists which are mutable.
+# Once a Tuple is created, it cannot be modified or changed further.
