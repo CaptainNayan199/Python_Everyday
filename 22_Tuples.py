6a3650157d2fc92c90680a396ac9519940a50515
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-11 19:56:21 +0545

    Files Modified

--- 22_Tuples.py
+++ 22_Tuples.py
@@ -1,2 +1,3 @@
 # Tuples is also one of the most important sequential data types in python
 # It is equally important as lists, and dictionaries(we'll talk abt it in future session).
+# They are order collection of data with each elements seperated by commas, and enclosed within parenthesis
