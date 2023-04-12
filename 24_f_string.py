e4d710df8356143941aadaccd6bbc42f5aeeece0
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-12 17:30:50 +0545

    Files modified

--- 24_f_string.py
+++ 24_f_string.py
@@ -1,3 +1,5 @@
 # Today we are going to see about fstring in python languages.
 # It is one of the best feature of python programming languages, and is appreciated by almost every py developers.
 # This was released after py 3.6 version
+
+# Fstring is string formatting in python languages
