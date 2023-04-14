b36b1547e13bfd3d53248dbe60d5514d8574927a
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-14 16:22:06 +0545

    FIles Updated

--- 25_Docstring_pep8'.py
+++ 25_Docstring_pep8'.py
@@ -1,6 +1,7 @@
 # Docstring in pythons are the string literals that appears right after definition of functions, classes, methods, etc.
+# Docstring are just like comments, 
 
-#using a docstring in functions
+#using a docstring in functions - 
 def cube (n):
     print(n*n*n)
 
