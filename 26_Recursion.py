a0288b24c3d026dfd95a0c9852583b3465b56bcf
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-14 16:47:08 +0545

    Files Updated

--- 26_Recursion.py
+++ 26_Recursion.py
@@ -1,2 +1,3 @@
 # Recursion is the method of repeating some task for n number of times, until the required or desired output is calculated.
 # Recursion function are those types of function in python that keeps on calling itsefl until the desired output is met. So is done by calling the function.
+# A recursive function should be able to call itself, have a proper stoppinfg condition or it will run infintely.
