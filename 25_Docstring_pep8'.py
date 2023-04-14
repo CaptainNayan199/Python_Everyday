758374e7b94973cc3ce5aa9baade6f3809ae2c47
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-14 16:25:01 +0545

    FIles Updated

--- 25_Docstring_pep8'.py
+++ 25_Docstring_pep8'.py
@@ -1,8 +1,9 @@
 # Docstring in pythons are the string literals that appears right after definition of functions, classes, methods, etc.
-# Docstring are just like comments, 
+# Docstring are just like comments, they are ignored by the interpreter but they can be printed/invoked/ or whatever you say.
 
 #using a docstring in functions - 
 def cube (n):
+    '''Hey this is my docstring, and I am inside a cube function. To use me please use  twice underscore doc twice underscore'''
     print(n*n*n)
 
 cube(3)
