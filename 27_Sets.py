75daa439c8738e822299e424280884e904489b2c
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-15 20:34:10 +0545

    Files Updated

--- 27_Sets.py
+++ 27_Sets.py
@@ -2,5 +2,5 @@
 # Sets are just the collection of unoredered objects.
 
 #  They are enclosed inside a curly braces, and are seperated by commas
-# They are immutable
+# They are immutable, that means we cannot change the content of the set once we have created it.
 
