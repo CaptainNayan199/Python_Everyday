547c7db3d34cb4b2ddc854a09625e7797d256962
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-15 20:36:02 +0545

    Files Updated

--- 27_Sets.py
+++ 27_Sets.py
@@ -1,5 +1,6 @@
 # SO what are sets in python ?
 # Sets are just the collection of unordered objects/well defined objects.
+# They dont take repeated values, and does not care about repeatation.
 
 #  They are enclosed inside a curly braces, and are seperated by commas
 # They are immutable, that means we cannot change the content of the set once we have created it.
