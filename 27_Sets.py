8c4b7391ec52fe8f8368c5a262ccb87224fe8ee2
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-15 20:38:22 +0545

    Files UPdated

--- 27_Sets.py
+++ 27_Sets.py
@@ -10,7 +10,7 @@ print(set1)
 
 set2 = {"Nayan", "23", "value", "this is set", False}
 print(set2)
-# set ignores the order.
+# set ignores the order, prints random values everytime.
 
 # PS : We cannot access the values of the set by the use of indexing methods.
 
