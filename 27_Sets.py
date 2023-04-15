4a2301887f12662cea22a3020f01167f34b7789b
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-15 20:33:51 +0545

    Files Updated

--- 27_Sets.py
+++ 27_Sets.py
@@ -1,4 +1,6 @@
 # SO what are sets in python ?
 # Sets are just the collection of unoredered objects.
- 
+
+#  They are enclosed inside a curly braces, and are seperated by commas
+# They are immutable
 
