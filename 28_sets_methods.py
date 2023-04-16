ace2a99c9526b91040a3ee1229be3e6996ffbe3b
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-16 21:18:58 +0545

    Updated

--- 28_sets_methods.py
+++ 28_sets_methods.py
@@ -1,4 +1,5 @@
 # In previous session we talked about sets, and today we are going to see about some important sets methods.
 
 set1 = {1,2,3,4}
-set2 = {5,6,7,8
+set2 = {5,6,7,8}
+print(set1+set2) 
