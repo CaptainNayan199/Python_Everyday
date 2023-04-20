a954c529e89891b23e71bf42699d7f03fbf8e827
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-20 21:23:57 +0545

    Files Updated

--- 28_sets_methods.py
+++ 28_sets_methods.py
@@ -4,4 +4,6 @@ set1 = {1,2,3,4}
 set2 = {5,6,7,8}
 print(set1+set2) # this is not the way to perform set operations
 # well lets see some of the operations that can be performed in the set like union, intersection, etc.
-print(set1.union(set2))
+print(set1.union(set2)) #this gives the union of two sets.
+
+
