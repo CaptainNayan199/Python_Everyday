7d0954f07a231559337678f4b207bcb357d5bd5e
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-20 21:30:16 +0545

    FIles Updated

--- 28_sets_methods.py
+++ 28_sets_methods.py
@@ -8,4 +8,4 @@ print(set1.union(set2)) #this gives the union of two sets.
 
 print(set1.intersection(set2)) #this gives intersection of two sets.
 
-(set1.update(set2)) # this method is saying that update the value of set1 i.e put every unique values of set2 in set1. SO our output will be: 1,2,3,4,5
+(set1.update(set2)) # this method is saying that update the value of set1 i.e put every unique values of set2 in set1. SO our output will be: 1,2,3,4,5,6,7,8
