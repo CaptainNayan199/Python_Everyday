e41274c7edf834e6f52d5bc0035a55c0649d1e7e
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-20 21:26:27 +0545

    Files Updated

--- 28_sets_methods.py
+++ 28_sets_methods.py
@@ -1,9 +1,11 @@
 # In previous session we talked about sets, and today we are going to see about some important sets methods.
 
-set1 = {1,2,3,4}
+set1 = {1,2,3,4,5}
 set2 = {5,6,7,8}
 print(set1+set2) # this is not the way to perform set operations
 # well lets see some of the operations that can be performed in the set like union, intersection, etc.
 print(set1.union(set2)) #this gives the union of two sets.
 
 print(set1.intersection(set2)) #this gives intersection of two sets.
+
+print(set1.update(set2)) # this method is saying that update the value of set1 i.e put every unique values of set2 in set1. SO our output will be 
