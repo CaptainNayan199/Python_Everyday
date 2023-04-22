fe5dfe1993d91d55f903cc0f7c7d5042dff68f60
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-22 13:29:37 +0545

    files updated

--- 28_sets_methods.py
+++ 28_sets_methods.py
@@ -82,6 +82,8 @@ s1 = {1,2,3,4,5,6}
 s2 = {8,9,10}
 print(s1.isdisjoint(s2)) # returns True as there are no any elements common in both the set.
 
-
+s1 = {1,2,3,4,5,6}
+s2 = {3,4,}
+print(s1.issuperset(s2)) #
 
 
