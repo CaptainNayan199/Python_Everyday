bf205964e0c5948456e31c1db6822dd53c9c31ea
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-22 21:46:26 +0545

    files updated

--- 31_For+Else.py
+++ 31_For+Else.py
@@ -7,3 +7,11 @@ else:
     print("Greater than 10") # this get printed after 9 as 10 is not satisfied in upper for loop, 
 
 
+for i in range(10):
+    print(i)
+    if i==4:
+        break
+else:
+    print("Hehehehe")
+
+# Now we used break in for loop, hence the loop is getting break, but else does not gets executed, because else only gets executed if the loop gets successfully, if loop gets break without finishing then the else statement does not gets gets executed.
