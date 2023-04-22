c1115b4d506e9a8cd5a2d295781e557e635fd43a
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-22 14:06:15 +0545

    files updated

--- 29_Dictionary.py
+++ 29_Dictionary.py
@@ -1,1 +1,4 @@
 # Dictionary in python are as similar as real world dictionary (oxford), it contains multiple keys with their respective values.
+# They are ordered collection of data.
+# It is the combination of key value pairs.
+# It is enclosed inside a curly braces as like set
