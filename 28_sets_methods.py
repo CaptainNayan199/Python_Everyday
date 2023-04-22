6dab6a28a59474627f8a5c1dc4a796dd0b665e88
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-22 13:33:59 +0545

    files updated

--- 28_sets_methods.py
+++ 28_sets_methods.py
@@ -98,5 +98,6 @@ s2 = {3,4,}
 print(s1) #this methods add the new elements, in the set
 #PS: add methods allows adding only single items at a time.
 
-#update methods - 
+#update methods - if we want to add multiple items inside of a set then there is this method
+
 
