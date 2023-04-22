e81e0873a69b59a3a0145d469cba621237128847
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-22 21:41:28 +0545

    files updated

--- 31_For+Else.py
+++ 31_For+Else.py
@@ -2,6 +2,6 @@
 
 
 for i in range(10):
-    print(i)
+    print(i) # now this will print upto 9 
 else:
-    print("Greater than 10")
+    print("Greater than 10") # this get printed after 9 as 10 is not satisfied in upper for loop, 
