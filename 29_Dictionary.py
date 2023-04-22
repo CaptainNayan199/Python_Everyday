25c5baa8a4beebc2689ebb67bf53960e955a7a8b
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-22 15:41:25 +0545

    files updated

--- 29_Dictionary.py
+++ 29_Dictionary.py
@@ -19,4 +19,27 @@ print(dic1) #this prints all the elements with keys as well as values.
 
 # we can access individual or desired elements too
 
+print(dic1["Name"])
+print(dic1.get("Name"))
+# we can use this two methods two get the values of the respected keys. Both are same method, but...
+
+#PS: the first methods throws error if key is not found, but the second method does not throws error.
+
+print(dic1["Nameee"]) # interpreter throws error saying key not found
+print(dic1.get("Nameee")) # returns None if key is not found, does not throw the error
+
+# they are like discard and remove method in sets, one throws error while other does not.
+
+# Accessing multiple keys / values from a dict : 
+
+
+dic1 = {"Name": "Nayan", "Age" : 20, "College": "TU", "isMarried": False
+}
+
+print(dict1.keys) # this returns all the keys present in the dictionary
+print(dict1.values) #this returns all the values present in the dictionary
+
+
+
+
 
