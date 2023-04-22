1dc04d365e33862db31af2fb72f5ef9a6f11719f
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-22 16:48:48 +0545

    file updated

---
+++ 30_Dict_methods.py
@@ -0,0 +1,11 @@
+# So today we will be discussing about some of the methods available in dictionary
+
+#Here i have one dictionary holding 
+dic = {
+    "Name": "Nayan",
+    "Aim": "Pilot",
+    "Age":20,
+    "isMarried": True,
+    "Friend": "Suman"
+}
+
