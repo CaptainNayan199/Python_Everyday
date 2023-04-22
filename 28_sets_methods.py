e26ad66c955da5f51bec4a79cd14ad31ec48db1d
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-22 13:39:28 +0545

    files updated

--- 28_sets_methods.py
+++ 28_sets_methods.py
@@ -114,7 +114,7 @@ s2 = {3,4,}
 s1.discard(1) # discards 1 from the set
 print(s1)
 
-#PS: remove and discard are not same method in 
+#PS: remove and discard same as well as not same also in python, if we try removing the 
 
 
 
