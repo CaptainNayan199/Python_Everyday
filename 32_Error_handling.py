c224a7ad090037f27dd27c5c8e660021f9fb0ab6
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-25 21:36:23 +0545

    files updated

--- 32_Error_handling.py
+++ 32_Error_handling.py
@@ -8,7 +8,10 @@ a = int(input("A number please: "))
 print(f"Multiplication table of {a} is : \n")
 for i in range(1,11):
     print(f"{a} X {i} = {a*i}")
+print("table has been printed")
+print("Other code")
 
 # so this is a simple program to generate multiplication table for any number in python
 # there are no errors in the above program
 # but if any errors can be occured in future, we can provide it in try and exception block
+# lets see this
