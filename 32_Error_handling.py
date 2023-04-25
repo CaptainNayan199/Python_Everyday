287fe2966aa6ece4dcf329aca6938344af939717
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-25 18:59:40 +0545

    files updated

--- 32_Error_handling.py
+++ 32_Error_handling.py
@@ -2,3 +2,6 @@
 #Similiarly it is same for python language
 # sometimes we knowingly or unknowingly are going to make an error, it can be because of user, or sometimes even because of server, so we can handle those errors in python language,
 # we can use try and exception in python langugage to handle errors and exception.
+
+
+a = int(input("A number please: "))
