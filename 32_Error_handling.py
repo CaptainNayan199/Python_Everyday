c2161147748a9b21e563ba671fce63fd4f01542b
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-25 18:59:04 +0545

    files updated

--- 32_Error_handling.py
+++ 32_Error_handling.py
@@ -1,4 +1,4 @@
 # Error handling is very important in any kinds of programming language
 #Similiarly it is same for python language
-# we can use try and 
 # sometimes we knowingly or unknowingly are going to make an error, it can be because of user, or sometimes even because of server, so we can handle those errors in python language,
+# we can use try and exception in python langugage to handle errors and exception.
