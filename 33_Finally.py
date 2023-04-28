db751ef5c4a50adc8d318c0e09c7ff95bb18af19
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-28 14:37:51 +0545

    files updated

--- 33_Finally.py
+++ 33_Finally.py
@@ -1,4 +1,4 @@
 # Previously we talked about error handlign by the use of try and except block
 # So today we will be talking about finally keyword in python
 # We can use finally with try and except keyword
-# PS : 
+# PS : Whatever the code is, or whatever error the code is having, finally always runs, no matter what.
