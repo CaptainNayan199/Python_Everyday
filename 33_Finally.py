ceb6281591cd36132435bbff3a513d22c01e4e5b
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-28 14:37:13 +0545

    files updated

--- 33_Finally.py
+++ 33_Finally.py
@@ -1,2 +1,4 @@
 # Previously we talked about error handlign by the use of try and except block
 # So today we will be talking about finally keyword in python
+# We can use finally with try and except keyword
+# PS : 
