7e9211348e6d22f8aa03ee711670d971df5a4171
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-29 20:34:59 +0545

    files updated

--- 34_custom_errors.py
+++ 34_custom_errors.py
@@ -1,6 +1,6 @@
 # Today in this session we will be looking at raising some custom errors in python
 # someimtes we need to raise custom errors.
-# we can use raise keyword in python for it.
+# we can use raise keyword in python.
 
 # why to raise error? - to hault the program and not cause any further damages to the code or similar to it.
 
@@ -9,3 +9,5 @@ if num<18:
     raise ValueError("You are too small") # sp here i have raised a custom error, saying if user gives me his age less than 18, then i will give him an error by saying you are too small.
 else:
     print("You are good to go")
+
+
