47b9f80a9933c11d158774d13aa262b4901ec74d
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-29 20:31:28 +0545

    files updated

--- 34_custom_errors.py
+++ 34_custom_errors.py
@@ -1,2 +1,5 @@
 # Today in this session we will be looking at raising some custom errors in python
 # someimtes we need to raise custom errors.
+# we can use raise keyword in python for it.
+
+# why to raise error? - to hault the program and not cause any further damages to the code or similar to it.
