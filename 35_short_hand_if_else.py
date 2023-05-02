7d754d8f3c9c6aa26028f0dfc3309f0b0a03db7f
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-05-02 21:43:17 +0545

    files updated

--- 35_short_hand_if_else.py
+++ 35_short_hand_if_else.py
@@ -6,3 +6,6 @@ print("Larger") if a>b else print("Smaller") if a<b else print("Equal") # it wil
 
 
 # this short hand statement will be useful when the condition is simple and code blocks are short to be executed
+
+
+num = 10 if a>b else 0 # will set the value of num as 10 if a>b else will set num = 0
