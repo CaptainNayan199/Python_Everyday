ecf81447331a81e658fff6d4b4073fd02c4a1c5b
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-05-02 21:38:39 +0545

    files updated

--- 35_short_hand_if_else.py
+++ 35_short_hand_if_else.py
@@ -1,6 +1,5 @@
 # so today in this session we will see about shorthand if else in python programming langauge
 
-
 a = 20
 b = 30
-print("Larger") if a>b else print("Smaller") if a<b else print("Equal")
+print("Larger") if a>b else print("Smaller") if a<b else print("Equal") # it will give smaller as output
