842641ab7f24b7987f69f5dc4cfead04e13f885a
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-05-02 22:13:55 +0545

    files updated

--- 36_Enumerate.py
+++ 36_Enumerate.py
@@ -55,7 +55,7 @@ for index, i in enumerate(temp, start=1):
 
 # so the indexing of the list will be - 1,2,3,4,5...., in place of 0,1,2,3,4,5....
 
-# so the enumerate function is often used when there is a need to loop over a sequence and perform
+# so the enumerate function is often used when there is a need to loop over a sequence and perform some actions
 
 # SO yeah this much for today, we will meet tomorrow, we will talk about enumeration in python.
 # Thank you! Happy coding.
