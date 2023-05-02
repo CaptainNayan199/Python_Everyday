ecaae3a35f9fd65acc657fefdc27bfff7f44993c
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-05-02 22:22:33 +0545

    files updated

--- 36_Enumerate.py
+++ 36_Enumerate.py
@@ -1,6 +1,7 @@
 
 # Enumerate is a built-in function in python that allows you to keep track of the number of iterations (loops) in a loop.
-# 
+# it is used to keep record of number of iterations in loop
+# enumerate function 
 
 temp = [30,33,34,36,38,40,46]
 index = 0
