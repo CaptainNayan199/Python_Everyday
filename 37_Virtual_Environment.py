d106d1a74b5c68d934d41dcab3774e643ea3e09a
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-05-03 22:00:08 +0545

    files updated

--- 37_Virtual_Environment.py
+++ 37_Virtual_Environment.py
@@ -35,4 +35,4 @@
 # pip freeze > text_file_name.txt - now it will create a new txt file that will store all our packages, modules and libraries information.
 
 
-# TIPS: What 
+# TIPS: What if we want to install 
