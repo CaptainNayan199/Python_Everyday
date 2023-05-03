1dbcbb998b4589c0110bad3f514721db94b9ec5b
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-05-03 21:13:48 +0545

    files updated

--- 37_Virtual_Environment.py
+++ 37_Virtual_Environment.py
@@ -9,3 +9,4 @@
 
 # How to install virtual environment in python?
 # Cmd - python -m venv virtual_env_name
+# python -m venv new_env - so here i have created a new python virtual environment with name new_env. Remember it is completely different from the 
