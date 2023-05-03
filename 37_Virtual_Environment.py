d34743f849dfbd57d4018f5b41ebbd5237fc22a8
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-05-03 21:07:17 +0545

    files updated

--- 37_Virtual_Environment.py
+++ 37_Virtual_Environment.py
@@ -1,3 +1,4 @@
 # so today we are going to see about virtual environment in python
 # So wht is virtual environment in python ? - in simple words virtual environment in python are python environment whrere libraries, scripts are isolated from the rest of the environment or default
 # virtual environment is very useful while managing different packages 
+# Using virtualenv allows you to avoid installing Python packages globally which could break system tools or other projects.
