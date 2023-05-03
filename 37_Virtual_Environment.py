c4bc47a5ab1ba6906503acaeaefd0202ec48b1d8
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-05-03 22:01:44 +0545

    files updated

--- 37_Virtual_Environment.py
+++ 37_Virtual_Environment.py
@@ -37,4 +37,4 @@
 
 # TIPS: What if we want to install older or rpevious libraries version
 # for eg : there is an library named xyz and its current version is 3.0
-# But what if i want to install previous 
+# But what if i want to install previous 2.0 version?
