bb00ef7805c39a29d0e440fded26f60a7b60901e
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-05-04 21:01:55 +0545

    code changed/files updated

--- 38_import.py
+++ 38_import.py
@@ -1,4 +1,4 @@
 # So today we are going to observe what actually is import and how does it work
 # what is the rocket science behind it?
 
-# so whenever we want to use any libraries in python, we need to install it using pip command, and then after installing it we need to import in our code
+# so whenever we want to use any libraries in python, we need to install it using pip command, and then after installing it we need to import in our code and then only we can use it.
