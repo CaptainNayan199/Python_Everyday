2eee9e36cc2ec0e0eb48b2d53e91ba1913fa726d
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-05-04 21:03:48 +0545

    code changed/files updated

--- 38_import.py
+++ 38_import.py
@@ -3,4 +3,4 @@
 
 # so whenever we want to use any libraries in python, we need to install it using pip command, and then after installing it we need to import in our code and then only we can use it.
 
-# IMporting in oython is the process of loading code from a python module in the current code. This allows us to use the function and variables defined in the module in your current code, as
+# IMporting in oython is the process of loading code from a python module in the current code. This allows us to use the function and variables defined in the module in your current code, as well as any aditional 
