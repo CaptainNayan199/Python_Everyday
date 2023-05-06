947174611a1812f2ed75d21e039c96f8170f770e
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-05-06 21:31:12 +0545

    Update 38_import.py

--- 38_import.py
+++ 38_import.py
@@ -2,7 +2,6 @@
 # what is the rocket science behind it?
 
 # so whenever we want to use any libraries in python, we need to install it using pip command, and then after installing it we need to import in our code and then only we can use it.
-
 # IMporting in oython is the process of loading code from a python module in the current code. This allows us to use the function and variables defined in the module in your current code, as well as any aditional modules that the imported module may depend on.
 
 # How to import a module or certain package in python?
