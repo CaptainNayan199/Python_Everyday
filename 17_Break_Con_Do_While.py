13e00148cbf7446c5e55abb613ce71d748e8fb26
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-05-08 22:03:02 +0545

    Update 17_Break_Con_Do_While.py

--- 17_Break_Con_Do_While.py
+++ 17_Break_Con_Do_While.py
@@ -1,6 +1,6 @@
 # Lopps in python run or work by each iteration and sometimes it can even rin for infinite times, and hence to stop that condition we should use break statement.
 # Break is used to forcefully exit the iteration, but not the loop itself, it just jumps over the next iteration.
-# Example : 
+# Example :  
 
 for i in range (12):
     if(i>9):
