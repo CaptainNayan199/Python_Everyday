4eeb20f098be52a200e6dd357c026bef70c1c9dc
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-05-11 21:34:33 +0545

    files updated

--- 38_import.py
+++ 38_import.py
@@ -17,4 +17,4 @@
 # From keyword in python - This keyword is used to import only a specified section in certain module
 # Eg - pip install math - i have imported a python module named math
 from math import pi # - so with the use of from keyword, i have imported a specified functions from the math package 
-# so from keyword is used to import certain or specified 
+# so from keyword is used to import certain or specified functions or variables from a module
