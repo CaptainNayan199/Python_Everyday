9067249048086a3d349b0e68b809fa6d5ff868a2
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-05-11 22:07:03 +0545

    files updated

--- 38_import.py
+++ 38_import.py
@@ -65,4 +65,4 @@ t.sum()
 
 # So yeah this much for today, today in this session we learned about import keyword 
 
-# Tomorrow we will learn about `if __name__ == __main__`
+# Tomorrow we will learn about `if __name__ == __main__` in python
