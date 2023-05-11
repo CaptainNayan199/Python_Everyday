2c85e56bccefc34fcba038f373fcc8b720784d22
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-05-11 22:04:02 +0545

    files updated

--- 38_import.py
+++ 38_import.py
@@ -59,4 +59,4 @@ print(t.name)
 t.sum()
 
 # So in this way import keyword works in python, import keyword is very important especially when working with modules and packages 
-# Whether we are working for web development, or working to process a certain image using frameworks, or in any other things
+# Whether we are working for web development, or working to process a certain image using frameworks, or in any other things we nned to use this import command
