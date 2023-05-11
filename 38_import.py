895612616262f9eefb4857db498b218a588815ee
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-05-11 21:36:44 +0545

    files updated

--- 38_import.py
+++ 38_import.py
@@ -21,4 +21,4 @@ from math import pi # - so with the use of from keyword, i have imported a speci
 
 # Importing everything with the use of * - this is used to import all the functions inside a certain modules
 
-from pandas import * # so here  
+from pandas import * # so here  i have imported all the contents either functions or variables from the modules named pandas
