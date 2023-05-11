1b541acd689e157559e4e84eb65e58dbec2bab72
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-05-11 22:06:11 +0545

    files updated

--- 38_import.py
+++ 38_import.py
@@ -63,4 +63,4 @@ t.sum()
 
 # So yeah! Not much hard stuffs here, the more you practice, the more you learn.
 
-# So yeah this much for today, today in this session we learned about import keyword
+# So yeah this much for today, today in this session we learned about import keyword 
