f8fa45c7d7066315584dde2aaed0578a38672f22
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-05-12 06:58:05 +0545

    files updated

--- 39_if__name__.py
+++ 39_if__name__.py
@@ -2,3 +2,5 @@
 # A Python programme uses the condition if __name__ == '__main__' to only run the code inside the if statement when the program is run directly by the Python interpreter. The code inside the if statement is not executed when the file's code is imported as a module.
 
 #  `if__name__` is a variable in python(inbuilt)
+
+# Python has a large number of special variables that begin and end with double underscores. They are referred to as dunder to keep it brief (from Double Underscores). In this case, "__name__" is pronounced "dunder name."
