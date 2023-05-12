919d49b81b472ec8abbad26b578fbdd88e0b84d1
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-05-12 06:56:17 +0545

    files updated

--- 39_if__name__.py
+++ 39_if__name__.py
@@ -1,3 +1,4 @@
 # if__name__ == "__main__" is a very important pattern in python programming 
+# A Python programme uses the condition if __name__ == '__main__' to only run the code inside the if statement when the program is run directly by the Python interpreter. The code inside the if statement is not executed when the file's code is imported as a module.
 
 #  `if__name__` is a variable in python(inbuilt) that will set the the name of the current module
