6eb4a8c0577b579a60e492098336cbcadcf82a80
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-05-12 13:46:47 +0545

    files updated

--- 39_if__name__.py
+++ 39_if__name__.py
@@ -6,3 +6,6 @@
 # Python has a large number of special variables that begin and end with double underscores. They are referred to as dunder to keep it brief (from Double Underscores). In this case, "__name__" is pronounced "dunder name."
 
 # print (__name__) # - __main__ will be the output as we are in our main python file, not inside a certain modules
+
+import time as time_package
+print(time_package.__name__) # now here i have imported a time module as time-package.
