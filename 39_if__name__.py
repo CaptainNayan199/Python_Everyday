3ec4b0f7d7e1528c5bed639a84d5a3b7a71ee265
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-05-13 21:37:13 +0545

    files updated

--- 39_if__name__.py
+++ 39_if__name__.py
@@ -20,3 +20,6 @@ import test as test_python_file
 test_python_file.sum(10,2) # now it will give me result as Sum is 12
 
 # Now if i do this than what happens ? 
+
+if __name__ == "__main__":
+    test_python_file.sum(10,2)
