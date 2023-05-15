b17ac90a54999680af49d107e8c0a593d5620b03
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-05-15 22:01:18 +0545

    Files updated

--- 40_OS_model.py
+++ 40_OS_model.py
@@ -1,3 +1,5 @@
 # So today we are going to see about OS module in python
 # This module is ued to interfere with OS of the computer and perform task accordingly
-# Its a built in module in python
+# Its a built in module in python 
+
+# PS : There are hundreds and hundreds of different methods inside 
