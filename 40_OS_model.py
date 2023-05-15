a9c00524ce82c3ace88db6434539f14d197a1cd2
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-05-15 22:03:06 +0545

    files updated

--- 40_OS_model.py
+++ 40_OS_model.py
@@ -9,3 +9,5 @@
 # The functions OS module provides allows us to operate on underlying Operating System tasks, irrespective of it being a Windows Platform, Macintosh or Linux.  - this is the main work done by OS module
 
 # Suppose i want to create a new folder , how can i do that?
+
+import os
