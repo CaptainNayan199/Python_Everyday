3ab1c9e4c8387dffeca21f7a7647149407085468
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-05-16 21:57:04 +0545

    files updated

--- 41_Loc_Glob_variables.py
+++ 41_Loc_Glob_variables.py
@@ -7,4 +7,4 @@
 
 def function():
     name="Nayan"
-    print("My name is ",name) # so here name is my local variable
+    print("My name is ",name) # so here name is my local variable, i cannot access it outside of function
