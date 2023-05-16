7d918af982dedd84a61a704b219e7cfcf073fd3e
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-05-16 21:53:44 +0545

    files updated

--- 41_Loc_Glob_variables.py
+++ 41_Loc_Glob_variables.py
@@ -1,4 +1,4 @@
 # So today we are going to see about local and global variables
 # We will also see about the use of global keyword, when to use it
 
-# So firstly 
+# So firstly lets see it in simple terms
