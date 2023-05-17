217f074390acbb77d142601e2e44b563d560ffe0
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-05-17 15:32:15 +0545

    files updated

--- 41_Loc_Glob_variables.py
+++ 41_Loc_Glob_variables.py
@@ -20,3 +20,5 @@ def function_2():
 
 # So, Global variable are those variables that are define out of any functions and are accessible through out the script i.e we can use this variable both in inside as well as outside of the function
 
+# But the case for 
+
