710ea96a19a97427b89e517ff52b1dea67a85bd9
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-05-17 15:31:03 +0545

    files updated

--- 41_Loc_Glob_variables.py
+++ 41_Loc_Glob_variables.py
@@ -17,3 +17,6 @@ name = "Ujjwal"
 def function_2():
     print("My name is ", name) # here name is a global variable and it is being used inside of a function
 
+
+# So, Global variable are those variables that are defined out
+
