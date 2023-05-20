14389c082dce5fc123b3a414911a65032c62df9f
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-05-20 17:48:50 +0545

    files updated

--- 42_File_IO.py
+++ 42_File_IO.py
@@ -1,3 +1,4 @@
 # So today we are going to see about file input and output in python
 # We can interfere with files using python in various mode like read, write,  and other as well.
 # Different file handling methods are used to interfere with files in python.
+
