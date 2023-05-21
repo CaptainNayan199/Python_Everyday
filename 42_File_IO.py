63267930ff7206ef8a4c7272ee7d7e98bc72f70b
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-05-21 07:56:17 +0545

    files updated

--- 42_File_IO.py
+++ 42_File_IO.py
@@ -22,6 +22,7 @@
 # r: open an existing file for a read operation.
 # w: open an existing file for a write operation. If the file already contains some data then it will be overridden but if the file is not present then it creates the new file as well.
 
+
 open_file = open("file_handling", 'r') # so basically this commmand will open a particular file in reading mode.
 
 # but yet we havent displayed the content of the file.
