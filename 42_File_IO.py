4721c7dd32bc4fd0f70052e7deca93f7a872cb62
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-05-21 08:20:12 +0545

    files updated

--- 42_File_IO.py
+++ 42_File_IO.py
@@ -53,7 +53,7 @@ with open("write.txt","w") as file: # this with keyword is use to automatically 
 
 # Appending a file - this method is used to insert or append something into the file
 
-# with open("file3.txt", "a+") as file:
+with open("file3.txt", "a+") as file:
    file.write("This is reading a file with a+ methods \n")
    data_inside_file = file.read()
     print(data_inside_file)
