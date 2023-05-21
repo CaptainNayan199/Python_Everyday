bf9a6413014083813d8007a93f29df805bb901eb
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-05-21 08:22:09 +0545

    files updated

--- 42_File_IO.py
+++ 42_File_IO.py
@@ -63,7 +63,7 @@ with open("file3.txt", "a") as file:
 
 # with open("file3.txt", "a+") as file:
     #file.write("This is reading a file with a+ methods \n")
-    #data_inside_file = file.read()
+    data_inside_file = file.read()
     print(data_inside_file)
 
 
