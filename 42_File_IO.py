fc6dde1f950ffe9fb09ccf1e65b5b1fd68590497
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-05-21 08:22:51 +0545

    files updated

--- 42_File_IO.py
+++ 42_File_IO.py
@@ -66,6 +66,10 @@ with open("file3.txt", "a+") as file: #appending new text
     data_inside_file = file.read()
     print(data_inside_file)
 
+#with open ("file3.txt", "r") as file:
+ #   data_inside_file = file.read()
+   print(data_inside_file)
+
 
 
  
