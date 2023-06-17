592b49de0ce383dbfb65c90833a0cfac78cf46c6
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-06-17 20:04:18 +0545

    files updated

--- 44_seek_&_oth_functions.py
+++ 44_seek_&_oth_functions.py
@@ -1,2 +1,8 @@
+# Playing with python
 # So today we are going to see about some functions in file handling like tell(), seek(), etc.
+
+# Doing miscellaneous things in python
+
+# seek() & tell() functions in python - this methods are used in python to play with various objects and theirs position within the file. They are built in functions, which makes file reading and writing bit easier and consistent.
+
 
