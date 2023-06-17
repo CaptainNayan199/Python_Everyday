4147414f904e809597a1d8c6ec621a76a9cd0503
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-06-17 18:33:56 +0545

    files updated

--- 43_read()_file_methods.py
+++ 43_read()_file_methods.py
@@ -1,1 +1,1 @@
-# So today we are going to see some important file methods in python like read(), readlines(), etc.
+# So today we are going to see some important file methods in python like read(), readlines(), writelines() etc.
