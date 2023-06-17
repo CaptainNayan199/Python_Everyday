4bea55c1c392f9a4bfafcd2516eaaa454da92d94
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-06-17 20:49:00 +0545

    files updated

--- 45_Lambda.py
+++ 45_Lambda.py
@@ -1,3 +1,5 @@
 # Lambda functions are very important functions in python, they are single expressions
 # They are used for writing anonymous functions
 # Anonymous functions are those functions that has no any name
+
+# Lambda functions syntax :  lambda arguments: expression
