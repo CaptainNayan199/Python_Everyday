bdeac4b17b430ef5f0a2224e9fa09a1cd426dc49
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-06-17 18:45:34 +0545

    files updated

--- 43_read()_file_methods.py
+++ 43_read()_file_methods.py
@@ -1,6 +1,6 @@
 # So today we are going to see some important file methods in python like read(), readlines(), writelines() etc.
 
-# readlines() methods - this methods is used to read a single line from the file, multiple lines can also be read by the use of loop
+# readlines() methods - this methods is used to read a single line from the file line by line, multiple lines can also be read by the use of loop 
 
 f = open("myfile.txt", 'r') #opening a file in read mode
 while True: #using while loop
