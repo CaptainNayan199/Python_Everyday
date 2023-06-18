3081d9428a4eeefad7469a1a9a20e6910680dd50
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-06-18 21:44:38 +0545

    files updated

--- 46_`IS`_`==`.py
+++ 46_`IS`_`==`.py
@@ -1,4 +1,4 @@
 # There is a keyword in python- is - what its use is? 
 # What is the difference between is and == in python, do they have similar meaning?
 
- 
+# Main difference betwwen 'is' and '==' is that 'is' compares the exact location of the object in the memory, whereas '==' compares the values
