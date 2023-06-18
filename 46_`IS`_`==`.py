e4cfdd0bf9b99338a4e71eab3e317987a220d9ff
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-06-18 21:44:57 +0545

    files updated

--- 46_`IS`_`==`.py
+++ 46_`IS`_`==`.py
@@ -2,3 +2,10 @@
 # What is the difference between is and == in python, do they have similar meaning?
 
 # Main difference betwwen 'is' and '==' is that 'is' compares the exact location of the object in the memory, whereas '==' compares the values
+
+
+a = 5
+if a is None: # I am asking 
+    print(True)
+else:
+    print(False)
