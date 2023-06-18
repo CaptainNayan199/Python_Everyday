8fc652451dfe4b430b8fd85df39be77f755b1950
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-06-18 18:41:17 +0545

    files updated

--- 45_map_filter.py
+++ 45_map_filter.py
@@ -1,2 +1,3 @@
-# So what are maps, filtering and reduce in python 
+# So what are maps, filtering and reduce in python
 # They are built in functions inside python programming language that basically allows us to apply a function to a sequence of elements and return a new sequence.
+# They are called as higher order functions, why is so, will see later.
