9edf9d14fb723a5b7687bd490247eb7524fe4d75
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-06-18 18:50:09 +0545

    files updated

--- 45_map_filter.py
+++ 45_map_filter.py
@@ -2,4 +2,14 @@
 # They are built in functions inside python programming language that basically allows us to apply a function to a sequence of elements and return a new sequence.
 # They are called as higher order functions, why is so, will see later.
 
-# Map functions - 
+# Map functions 
+
+def square(a): # a function that returns a square of a number
+    return a*a
+
+
+list1 = [1,2,3,4,5] # an list
+sq_list = [] # an empty list
+for i in list1: #calculating the square of a list using for loop
+    sq_list.append(square(i)) #appending the square in new list
+print(sq_list)
