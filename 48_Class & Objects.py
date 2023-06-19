a33cc1482fe8ef6919018a9bef5eb51c7e9076ea
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-06-19 19:33:04 +0545

    files updated

--- 48_Class & Objects.py
+++ 48_Class & Objects.py
@@ -43,3 +43,4 @@ obj.infoo()
 # Self is passed as parameter in the infoo method ! But why ?
 # self parameter - It is an current instance of the class, and is used to access variables that belongs to the class, It is a must that self must be provided as parameter inside the method definitions inside the class.
 
+# I can make multiple objects of an class
