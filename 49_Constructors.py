69ff9dd06c78c08cfba169d03f5a4218bacd2536
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-06-19 21:17:46 +0545

    files updated

--- 49_Constructors.py
+++ 49_Constructors.py
@@ -1,6 +1,3 @@
 # A constructor is a special type of member function that is called automatically when an object is created
 # They help in creating an object, helps in initialization
 
-
-class Cons:
-    
