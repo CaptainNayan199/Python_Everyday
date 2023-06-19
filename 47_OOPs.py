4288005b291c5583fb3dcd383313c3e6ca80b507
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-06-19 15:21:28 +0545

    files updated

--- 47_OOPs.py
+++ 47_OOPs.py
@@ -1,3 +1,4 @@
 # There are two types of programming language(in general); one is procedure oriented and the other is object oriented
 
-# POP - it follows top to bottom approach, runs sequentially
+# POP - it follows top to bottom approach, runs sequentially, use functions mainly.
+# OOP - it follows bottom up approach, use of real world entites called as objects, class
