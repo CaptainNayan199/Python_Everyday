bab3793cefc84654e2b3cd9f44365c8010e11685
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-06-19 21:18:27 +0545

    files updated

--- 49_Constructors.py
+++ 49_Constructors.py
@@ -1,3 +1,10 @@
 # A constructor is a special type of member function that is called automatically when an object is created
 # They help in creating an object, helps in initialization
 
+
+class Cons:
+    name= "Nayan"
+    age = 20
+
+    def Data(self):
+        print(f"Name is {self.name} and age is {self.age}")
