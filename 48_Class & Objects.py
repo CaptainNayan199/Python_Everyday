47a291aafba65827609176174815c57152824635
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-06-19 15:43:32 +0545

    New files added

--- 48_Class & Objects.py
+++ 48_Class & Objects.py
@@ -1,1 +1,3 @@
 # As talked in previous session classes are blue prints of real world, and objects are instance of an class
+
+# Creating a class in python : class can be created in python by the use of class keyword.
