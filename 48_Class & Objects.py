ecb37280f0741f63c472d76729ad094568fc1d9e
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-06-19 17:57:52 +0545

    files updated

--- 48_Class & Objects.py
+++ 48_Class & Objects.py
@@ -40,4 +40,5 @@ obj.age = int(input("Enter your age : "))
 obj.addr = input("Enter your current address : ")
 obj.infoo()
 
-# Self is passed as argument in the infoo method ! But why ?
+# Self is passed as parameter in the infoo method ! But why ?
+# self parameter - 
