cd4534bc3dade895508b4b771bd0be44e845c697
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-06-21 21:06:07 +0545

    files updated

--- 50_Decorators.py
+++ 50_Decorators.py
@@ -1,2 +1,3 @@
 # So what actually is decorators in python?
 # They are special types of function that takes a function as an argument and is used to add more functionality to a function
+# It is one 
