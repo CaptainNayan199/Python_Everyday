d55693261e0e095f2a62621992d45bb2f3c2b2f7
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-06-22 07:57:30 +0545

    files updated

--- 50_Decorators.py
+++ 50_Decorators.py
@@ -3,4 +3,4 @@
 # It is one of the most important topic in python that allows us to modify the behavior of the methods, without actually modifying the methods.
 # They change and return the function that is provided as arguments.
 
-# 
+# They are a powerful and versatile tool that allows us to modify the behavior of the function and 
