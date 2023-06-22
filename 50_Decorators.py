acbe5f54961034706ed6a93c803e2e134f6bb322
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-06-22 07:56:49 +0545

    files updated

--- 50_Decorators.py
+++ 50_Decorators.py
@@ -2,3 +2,5 @@
 # They are special types of function that takes a function as an argument and is used to add more functionality to a function
 # It is one of the most important topic in python that allows us to modify the behavior of the methods, without actually modifying the methods.
 # They change and return the function that is provided as arguments.
+
+# 
