6e0003b71a7e5197518fab958f3e4c18a198f694
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-07-02 20:22:31 +0545

    files updated

--- 50_Decorators.py
+++ 50_Decorators.py
@@ -24,6 +24,6 @@ func = decorator_function(func)
 # Here i have a decorator function that takes 1 arguments
 
 def decor_func(func):
-    # inside this function i will create another function
+    # inside this function i will create another function that will be used to 
 
 # A simple program that prints hello world
