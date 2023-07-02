8276077ec03086802ec95dc3758043cbc1d88cfe
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-07-02 20:23:55 +0545

    files updated

--- 50_Decorators.py
+++ 50_Decorators.py
@@ -28,5 +28,6 @@ def decor_func(func):
     def inner_func():
         print("Before hello world this will be printed")
         func() #the same function that has been passed as an parameter will be executed here
+        print("After hello world this will be printed")
 
 # A simple program that prints hello world
