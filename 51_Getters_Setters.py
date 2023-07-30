ddf59dc199069d4078203b3f51c3fcd0b5f52ba8
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-07-30 14:02:44 +0545

    files updated

--- 51_Getters_Setters.py
+++ 51_Getters_Setters.py
@@ -1,3 +1,3 @@
 # A function that are used to set any values (return) for any function can be referred to as getters and setters
 
-# Getters in python - It is used to access the value of an object properties.
+# Getters in python - It is used to access the value of an object properties. They are used to return the value of a specific property. They are defined using the @property decorator.
