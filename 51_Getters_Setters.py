c69c34cb2414bf25c55a23594a56496e813a02dc
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-08-26 10:29:05 +0545

    files updated

--- 51_Getters_Setters.py
+++ 51_Getters_Setters.py
@@ -4,6 +4,8 @@
 
 # In Python property()is a built-in function that creates and returns a property object. A property object has three methods, getter(), setter(), and delete()
 
+# Python @property is one of the built-in decorators. The main purpose of any decorator is to change your class methods or attributes in such a way so that the user of your class no need to make any change in their code. 
+
 
 class Myclass:
     def __init__(self, value):
