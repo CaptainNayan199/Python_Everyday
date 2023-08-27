b68b445c549ec3462402c77b810f2ff7569b78eb
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-08-27 19:53:19 +0545

    files updated

--- 51_Getters_Setters.py
+++ 51_Getters_Setters.py
@@ -27,3 +27,6 @@ print(obj.age) #here i will not get any error coz i an accessing a normal public
 # Thsi process works for functions inside class as wells
 print(obj.age) #here i will not get any error coz i an accessing a normal public unprotected variable by the use of object 
 # Thsi process works for function inside class sxas well
+
+
+# if we want to encapsulated variable than we can do by the use of methods or constructor
