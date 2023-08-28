5e2024578795dc061a0e140f7748401f7b15fc94
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-08-28 14:02:58 +0545

    files updated

--- 52_*args.py
+++ 52_*args.py
@@ -4,7 +4,7 @@
 # *args is a parameter that accpets multiple non keyword arguments
 # whereas, **kwargs accepts non keyword arguments, we will talk about that later.
 
-# *args will pack all the arguments that it gets inside a tuple
+# *args will pack all the arguments that it gets inside a tuple( ordered and unchangeable)
 
 # Lets see about keyword arguments and non keyword arguments
 
