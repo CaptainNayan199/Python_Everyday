696cb2dd0f6e94384608e11dbdefd2c40fcf67c9
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-08-28 13:51:56 +0545

    files updated

--- 52_*args.py
+++ 52_*args.py
@@ -3,3 +3,7 @@
 #  so *args is important when we have to accept multiple and differenct types of variables
 # *args is a parameter that accpets multiple non keyword arguments
 # whereas, **kwargs accepts non keyword arguments, we will talk about that later.
+
+# *args will pack all the arguments that it gets inside a tuple
+
+# Lets see about keyword arguments and non keyword arguments
