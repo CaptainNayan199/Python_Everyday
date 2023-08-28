80c205fd1dd71b5823349b23eba2c6ee53d95fc4
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-08-28 13:43:24 +0545

    files updated

--- 52_*args.py
+++ 52_*args.py
@@ -1,3 +1,4 @@
 # So lets talk about *args in python
 #  args - means arguments
 #  so *args is important when we have to accept multiple and differenct types of variables
+# *args is a parameter that accpets multiple non keyword arguments
