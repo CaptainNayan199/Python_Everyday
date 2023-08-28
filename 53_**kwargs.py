01aaa096dffb6450ef3f345b185fccb0775dc113
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-08-28 14:21:42 +0545

    files updated

---
+++ 53_**kwargs.py
@@ -0,0 +1,5 @@
+
+# **kwargs - same as *args, but will pack all the arguments into a dictionary, and is basically for keyword arguments, very useful when we need to accept multiple and different dta types key word arguments
+
+
+# kwargs - keyword arguments
