920cbf5a82ba6c0e13251feeb1eea4b0d05635e0
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-08-28 14:43:11 +0545

    files updated

--- 53_**kwargs.py
+++ 53_**kwargs.py
@@ -10,8 +10,9 @@
 
 # Eg 1 : 
 
-def price(**kwargs):
+def price(**kwargs): # all the arguments will be packed as dictionary with keys and respective values
+
     for key, value in kwargs.items():
         print("Price of %s is %s" % (key, value))
 
-price(Apple=200, Mango=400, Grapes=500, Kiwi=1000)
+price(Apple=200, Mango=400, Grapes=500, Kiwi=1000) # key word arguments
