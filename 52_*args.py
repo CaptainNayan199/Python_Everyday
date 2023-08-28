ed8c271e15b145fbfcdfc2d2546b146a075f23a8
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-08-28 14:22:35 +0545

    files updated

--- 52_*args.py
+++ 52_*args.py
@@ -44,4 +44,4 @@ def std_info(*name):
 std_info("Nayan", 20, "MMC")
 
 # So yeah this much for today, in next session we will see about **kwargs in brief.
-# Thank you! Happy coding
+# Thank you! Happy coding.
