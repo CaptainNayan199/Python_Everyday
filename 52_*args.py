f5ada0e0fa41fe1ee616854352f1d38997fb0614
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-08-28 13:43:56 +0545

    files updated

--- 52_*args.py
+++ 52_*args.py
@@ -2,3 +2,4 @@
 #  args - means arguments
 #  so *args is important when we have to accept multiple and differenct types of variables
 # *args is a parameter that accpets multiple non keyword arguments
+# whereas, **kwargs accepts non keyword arguments, we will talk about that later.
