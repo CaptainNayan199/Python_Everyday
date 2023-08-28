19df6ab37ee4ccbc4f9a883e52783fab8e4fcbe1
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-08-28 14:42:33 +0545

    files updated

--- 53_**kwargs.py
+++ 53_**kwargs.py
@@ -6,3 +6,12 @@
 # kwargs - keyword arguments
 
 # One can think of the kwargs as being a dictionary that maps each keyword to the value that we pass alongside it. That is why when we iterate over the kwargs there doesn’t seem to be any order in which they were printed out.
+
+
+# Eg 1 : 
+
+def price(**kwargs):
+    for key, value in kwargs.items():
+        print("Price of %s is %s" % (key, value))
+
+price(Apple=200, Mango=400, Grapes=500, Kiwi=1000)
