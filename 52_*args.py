8e88d4bf3d37ee80dbbca1140f02e7002f14f30d
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-08-28 13:53:04 +0545

    files updated

--- 52_*args.py
+++ 52_*args.py
@@ -16,4 +16,4 @@ def std_info(name, age, cllg, sem, num): # a function that has several parameter
     print("He is in ", sem, " sem")
     print("His contact number is ", num)
 
-std_info("Nayan Pathak",20,"MMC","4th",9816944821)
+std_info("Nayan Pathak",20,"MMC","4th",985432523533) #
