a44712b77a435df10f084885e9a023df0975b406
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-08-29 11:44:41 +0545

    files updated

--- 54_Inheritance.py
+++ 54_Inheritance.py
@@ -1,3 +1,4 @@
 # So today we will see about inheritance in python
 # Inheritance is a very important topics in OOP.
 # So what inheritance actually is ? ---> so inheritance is a key feature in OOP where a class can inherit methods, and properties of another class
+# The same is in python as well, theres a parent class and we want to access its properties in another child class, to do so we need inheritance concepts.
