e60824860079251e35371d31e0874928353dae4e
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-08-30 16:32:51 +0545

    files updated

--- 55_Access_modifier.py
+++ 55_Access_modifier.py
@@ -1,4 +1,4 @@
 # Today we are going to see about access modifie. So what are access modifiers?
 # As name says, they are used to set the accessibility  or visibility of any variables, objects, methods, etc.
 # There are 3 types of access modifiers in python. They are public which is the default one, private, and protected.
-# These access modifiers provide restrictions on the access of member variables and methods of the class from any object outside the class
+# These access modifiers provide restrictions on the access of member variables and methods of the class from any object outside the class.
