241aeaec25c0e89abadf78f7e857457310735036
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-09-23 14:48:30 +0545

    files updated

--- 55_Access_modifier.py
+++ 55_Access_modifier.py
@@ -29,3 +29,14 @@ obj = Nayan()
 obj.myname()
 print(obj.__name) #now this will give error, as we cannot directly access the private attributes of a class.
 # But it can be accessed in indirect ways
+
+class Nayan:
+    def myname(self):
+        self.__name = "Nayan"
+        
+
+obj = Nayan()
+obj.myname()
+print(obj._Nayan__name) #So this is called as name-mangling approach in python, typically this is not consider good practice but yeah knowing it might be a good work as well.
+
+# In Python, when you prefix an instance variable with a double underscore like __name, Python performs name mangling to make it more difficult to accidentally override the attribute in subclasses. Name mangling adds a prefix to the variable name, based on the class name, to make it unique.
