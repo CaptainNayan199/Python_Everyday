0e0749f169d1ff5f74bf0e9a544f955beef1d0cc
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-09-24 13:03:38 +0545

    files updated

--- 56_Static.py
+++ 56_Static.py
@@ -1,1 +1,2 @@
 # So today we are going to see about static methods in python. What are they and what do they do ?
+# Static methods can be directly called, no need to make an instance, even though they lie inside class.
