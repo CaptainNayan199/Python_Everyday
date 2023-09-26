5805c2144df957302b30a475ac8c142aa59b66e5
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-09-26 11:21:08 +0545

    files updated

--- 57_Var(inst vs class).py
+++ 57_Var(inst vs class).py
@@ -1,2 +1,4 @@
 # Variables can be classified as instance and class level in python. They can be defined in class level and at the instance level
 # So what are they? How to understand them ?
+
+# Class variables : 
