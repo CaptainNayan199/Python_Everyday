f513ee065994619df48d228150e3b293309e8550
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-09-27 13:12:36 +0545

    MyClass.class_method(5)

--- 58_class_methods.py
+++ 58_class_methods.py
@@ -1,6 +1,6 @@
 # class methods are special kinds of methods that belongs to class itself, rather than to any instances of that class. It is a special kind of class function that is only work the class, not for the objects created from the class.
 # They are defined inside a class, just like regular methods.
-# The can be called inside a class, without need to define or create an object
+# The can be called outside a class, without need to define or create an object
 
 # Why to create class methods ? 
 # To modify class level variables
