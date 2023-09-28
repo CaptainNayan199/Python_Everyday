f70ed202ffcef3aeb9723c9ca748cc6b40a2d26a
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-09-28 13:25:54 +0545

    files updated

--- 59_CM_as_Constructors.py
+++ 59_CM_as_Constructors.py
@@ -4,3 +4,31 @@
 # Think of a class methods as a way to create class objects in a different way.
 
 # Using class methods as additional constructors can make your code more flexible and user-friendly, allowing you to create objects from a class in various ways depending on your specific needs.
+
+
+
+class People:
+
+  def __init__(self, name, age):  #constructor
+    self.name = name
+    self.age = age
+
+  @classmethod
+  def birthYear(cls, name, birthYear):
+    agee = cls.findAge(birthYear)  #calling the static method
+    return cls(name, agee) #invoking the class constructor with two parameters
+
+  @staticmethod
+  def findAge(birthYear):
+    current_year = 2023
+    age = current_year - birthYear
+    return age
+
+
+# Creating an object by default way
+People1 = People("Nayan", 21)
+print(f"My name is {People1.name} and my age is {People1.age}")
+
+# Now lets use class method
+People2 = People.birthYear("Ujjwal", 2002)
+print(f"My name is {People2.name} and my age is {People2.age}")
