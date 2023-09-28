eb5e9bd048dedaf68a1d1f9c10bcdae2f6eabaec
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-09-28 14:08:32 +0545

    files updated

--- 60_dir_dict_help.py
+++ 60_dir_dict_help.py
@@ -1,4 +1,4 @@
 # Today we are going to see about some important methods that are used mostly in python.
 # We will be looking at dir(), __dict__, help() methods for today. Well __dict__ is attributes, not actually a methods. So let's see about them in detail.
 
-# dir() methods - it is a built-in function in python that is used for object introspection(means looking what operation we can perform on objects)
+# dir() methods - it is a built-in function in python that is used for object introspection(means looking what operation we can perform on objects). 
