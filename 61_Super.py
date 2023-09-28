69262e80572da919772cda22cfaad63312143e48
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-09-28 14:56:55 +0545

    files updated

--- 61_Super.py
+++ 61_Super.py
@@ -1,6 +1,7 @@
 # In Python, the super() keyword is used to call methods or access attributes from a parent or superclass within a subclass. It allows you to inherit and reuse code from the parent class. 
 
 class Parent:
+  name = "Nayan"
   def pMethod(self):
     print("I am parent method")
 
@@ -9,6 +10,7 @@ class Child(Parent):
     print("I am child method")
     Parent().pMethod() #this method also can access the parent method
     self.pMethod() #this method can also access the parent method
+    print(self.name) #this method can access the attributes of the parent class.
 
 
 child_obj  = Child()
