771bb2bcfac2f3da476205350e5e45e87afa911f
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-09-28 14:04:12 +0545

    files updated

--- 60_dir_dict_help.py
+++ 60_dir_dict_help.py
@@ -1,2 +1,4 @@
 # Today we are going to see about some important methods that are used mostly in python.
-# We will be looking at dir(), __dict__, help() methods for today. Well __dict__ is attributes, not actually a methods
+# We will be looking at dir(), __dict__, help() methods for today. Well __dict__ is attributes, not actually a methods. So let's see about them in detail.
+
+# dir() methods - 
