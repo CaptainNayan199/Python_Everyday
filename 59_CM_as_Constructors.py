40224edb59eaffc43619f2fab7b4220c0bf07744
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-09-28 13:28:36 +0545

    files updated

--- 59_CM_as_Constructors.py
+++ 59_CM_as_Constructors.py
@@ -34,4 +34,5 @@ People2 = People.birthYear("Ujjwal", 2002)
 print(f"My name is {People2.name} and my age is {People2.age}")
 
 # So yeah this is it. Today we learned how we can use classmethods as aditional constructor as well.
-# Tomorrow 
+# Tomorrow we will be looking at some methods in python that are very important
+
