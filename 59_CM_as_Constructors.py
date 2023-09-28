191cd6da5d404f222b52bc110005b2073c2e72e6
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-09-28 12:30:38 +0545

    files updated

--- 59_CM_as_Constructors.py
+++ 59_CM_as_Constructors.py
@@ -1,2 +1,3 @@
 # In previous session we talked about class methods - they are those types of methods that belongs/bounds to the class itself rather than to the instances of the class.
 # One of the most prominent use of class method in python is as aditional constructors. 
+# We can set the values of the constructors using the class methods
