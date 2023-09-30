e4dbdf1dc2f65197e7bcc4631ca83f9c735c1c3e
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-09-30 15:14:09 +0545

    files updated

--- 62_Dunders.py
+++ 62_Dunders.py
@@ -4,3 +4,5 @@
 # Dunder, or magic methods in Python, are special methods that start and end with double underscores (e.g., __init__, __str__, __add__). These methods have special meanings and are used to define how objects of a custom class behave in various situations.
 
 # Let's see with some examples: 
+# 1 __init__(self, ...): This method is called when you create a new object from a class. It initializes the object's attributes. It is an constructor, we all know about it.
+
