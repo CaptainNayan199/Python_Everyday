3e304d060922878ecb1854dc4528708d312b2283
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-09-30 15:12:17 +0545

    files updated

--- 62_Dunders.py
+++ 62_Dunders.py
@@ -1,2 +1,4 @@
 # So what are dunder methods? or what are magic methods ?
 # We have been using them for so long period of time, but we dont know abt thems
+
+# Dunder, or magic methods in Python, are special methods that start and end with double underscores (e.g., __init__, __str__, __add__). These methods have special meanings and are used to define how objects of a custom class behave in various situations.
