89a943cf4d6759504cae53da06616dc5ebda1c04
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-09-30 15:32:17 +0545

    files updated

--- 62_Dunders.py
+++ 62_Dunders.py
@@ -39,3 +39,36 @@ print(length)
 
 # These dunder/magic methods provide a way to customize the behavior of your custom objects, making your code more readable and intuitive when working with instances of your classes.
 # So this is it. There are tons of dunders methods in python but but but..
+# Some of the popular and widely used dunder methods in python are : 
+
+# # __init__: Constructor method, called when you create a new object.
+# __str__: Called by the str() function and print() to represent an object as a string.
+# __repr__: Used to represent an object as a string for debugging purposes.
+# __len__: Defines the behavior of the len() function for objects of the class.
+# __getitem__: Enables indexing like obj[key].
+# __setitem__: Enables assignment via indexing like obj[key] = value.
+# __delitem__: Enables item deletion using del obj[key].
+# __iter__: Defines how objects are iterated over in a loop.
+# __next__: Used in conjunction with __iter__ to define iteration behavior.
+# __contains__: Allows you to use the in keyword to check for item existence.
+# __eq__: Defines equality behavior (used with ==).
+# __ne__: Defines non-equality behavior (used with !=).
+# __lt__: Defines less-than behavior (used with <).
+# __le__: Defines less-than-or-equal-to behavior (used with <=).
+# __gt__: Defines greater-than behavior (used with >).
+# __ge__: Defines greater-than-or-equal-to behavior (used with >=).
+# __add__: Defines behavior for the + operator.
+# __sub__: Defines behavior for the - operator.
+# __mul__: Defines behavior for the * operator.
+# __truediv__: Defines behavior for the / operator (true division).
+# __floordiv__: Defines behavior for the // operator (floor division).
+# __mod__: Defines behavior for the % operator (modulo).
+# __pow__: Defines behavior for the ** operator (exponentiation).
+# __abs__: Defines behavior for the abs() function.
+# __call__: Allows an instance to be called as a function.
+# __enter__ and __exit__: Used for context management with the with statement.
+# __getattr__: Called when an attribute is accessed but not found.
+# __setattr__: Called when an attribute is set.
+# __delattr__: Called when an attribute is deleted.
+# __hash__: Defines the object's hash value for use in hash-based collections.
+# __instancecheck__ and __subclasscheck__: Used for type checking and isinstance/issubclass checks.
