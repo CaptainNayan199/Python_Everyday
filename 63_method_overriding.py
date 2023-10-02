a0288fe03546b07ed8c71e64250b3052df14bab0
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-10-02 08:08:42 +0545

    files updated

--- 63_method_overriding.py
+++ 63_method_overriding.py
@@ -1,2 +1,1 @@
 # Method overriding in python is a concept of oop where a subclass provides a specific implementation for a method that is already defined in its parent class. This allows the subclass to customize or extend the behavior of the inherited method while keeping the same method name.
-
