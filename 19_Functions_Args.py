6337da4a6be882e13b13bac6dddf46eb7e539602
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-10-06 19:41:47 +0545

    Update 19_Functions_Args.py

--- 19_Functions_Args.py
+++ 19_Functions_Args.py
@@ -2,6 +2,7 @@
 # Functional arguments are placed inside a small brakcets or ps: in python it can be given in form of sets, dictionaries too. (dont worry abt this things, we will see them in brief in upcoming sessions)
 #  Lets see the different types of arguments in python programming
 
+
 # 1. Default Argument 
 def sum (a=3, b=4):
     print("Sum is ", a+b)
