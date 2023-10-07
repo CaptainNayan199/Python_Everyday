958cc461e9e44a0c3c0288dcfd3bc5ebd7787ace
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-10-07 20:22:17 +0545

    Update 10_String_slicings.py

--- 10_String_slicings.py
+++ 10_String_slicings.py
@@ -2,6 +2,7 @@
 # we can slice stirngs from forwad as well as backward of a string
 # Slicing string from backward can be referred to as negative slicing
 
+
 # Each element indexing starts from 0
 # For eg: name = "Nayan", here the indexing of N=0, a=1, y=2, a=3, n=4
 
