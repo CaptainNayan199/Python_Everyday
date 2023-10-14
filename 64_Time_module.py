4e32f3380b72f1b3f04d8aca2b1c1c3adbde4201
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-10-14 16:42:14 +0545

    files updated

--- 64_Time_module.py
+++ 64_Time_module.py
@@ -1,5 +1,11 @@
 # So today we are going to learn about time module in python
 # This module is a built in function/module in python that allows us to work with times and perform times related operations.
-# Let's see in depth
+# The time-related tasks includes : 
+  # reading the current time
+  # formatting time
+  # sleeping for a specified number of seconds and so on.
+  # Let's see in depth
 
 # First to use time module you need to import time module.
+
+pip install time
