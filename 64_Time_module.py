385886e11f3005ed499b07a159137058a57374ec
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-10-14 16:33:13 +0545

    files updated

--- 64_Time_module.py
+++ 64_Time_module.py
@@ -1,2 +1,2 @@
 # So today we are going to learn about time module in python
-# This module is a built in function/module in python that allows us to work with times and perform times
+# This module is a built in function/module in python that allows us to work with times and perform times related operations.
