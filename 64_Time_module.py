5262d75399681313d58201731734ecbb22ff2674
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-10-14 17:53:48 +0545

    files updated

--- 64_Time_module.py
+++ 64_Time_module.py
@@ -47,4 +47,4 @@ print(time.strftime("%m/%d/%Y , %H:%M:%S"))  #10/14/2023 , 11:24:11
 # %M - minutes [00, 01, ..., 58, 59]
 # %S - second [00, 01, ..., 58, 61]
 
-# There are lots and lots of things inside a time module. Above were some basics. 
+# There are lots and lots of things inside a time module. Above were some basics. You can check it in documentation as well.
