d3a135ee61c8d9c5ad886aaf6d8f7090f8bde1a8
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-10-15 16:03:02 +0545

    files updated

--- 65_walrus.py
+++ 65_walrus.py
@@ -1,2 +1,4 @@
 # Wlarus operator is a new thing in python. It came in practice after python 3.8 version. That means python version older than 3.8 does not support walrus operator. It is very useful operator in python.
 # So this operator allows us to assign value to the variable within an expression.
+
+# The Walrus Operator is represented by the := syntax and can be used in a variety of contexts including while loops and if statements.
