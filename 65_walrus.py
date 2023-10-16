399a7a030d8c7c6566887ee6a23f99e4f0121655
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-10-16 13:25:56 +0545

    files updated

--- 65_walrus.py
+++ 65_walrus.py
@@ -53,4 +53,4 @@ while True:
         break
     food_list.append(fruit)
 print(f"Your favourite fruit are : {food_list}")
-# A simple program where theres en empty list, and user are asked about their favourite foods and is entered into the list. No problem right ? Now let's rewrite this program
+# A simple program where theres en empty list, and user are asked about their favourite foods and is entered into the list. No problem right ? Now let's rewrite this program by the use of walrus operator
