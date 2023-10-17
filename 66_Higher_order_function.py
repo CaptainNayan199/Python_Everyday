6ee8ec123ccf769c324ade9a189143f841046fe5
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-10-17 16:06:09 +0545

    files updated

--- 66_Higher_order_function.py
+++ 66_Higher_order_function.py
@@ -1,3 +1,4 @@
 # So today we are going to see about some higher order functions and what are they ?
 # We all know that everything in python is treated as an object. So is functions.
 # Higher order functions are those types of function in python where a function is accepting another function as an argument or is returning some function(ofcourse we have seen this in decorators as well, they return a function right?)
+# So let's see with an example
