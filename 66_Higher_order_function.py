dbc053a75de3643f55552e259c667102347347ba
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-10-17 16:05:13 +0545

    files updated

--- 66_Higher_order_function.py
+++ 66_Higher_order_function.py
@@ -1,3 +1,3 @@
 # So today we are going to see about some higher order functions and what are they ?
 # We all know that everything in python is treated as an object. So is functions.
-# Higher order functions are those types of function in python where 
+# Higher order functions are those types of function in python where a function is accepting another
