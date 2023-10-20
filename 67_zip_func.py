1dbf744b026da03513cec653b85ede2585066560
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-10-20 16:51:43 +0545

    files updated

--- 67_zip_func.py
+++ 67_zip_func.py
@@ -1,3 +1,3 @@
 # So today we are going to see about a popular method in python, that is zip function.
 # So what is zip function ?
-# zip function is a special type of method in python that returns a zip object which is actually an iterator of tuples or list
+# zip function is a special type of method in python that returns a zip object which is actually an iterator of tuples or list or any other iterable object in python
