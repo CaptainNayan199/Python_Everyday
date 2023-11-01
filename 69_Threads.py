31e8ae568843e40d4e40a4e9d52ccddfa36f1ded
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-11-01 10:13:27 +0545

    files updated

--- 69_Threads.py
+++ 69_Threads.py
@@ -6,7 +6,7 @@
 
 # In python there is a built in module for thread called as `threading`, using it we can create a new thread, manage them, and also communicate between them.
 
-# But PS: in multi threaded python program, only one thread can execute at a time even if the cpu has multiple cores. It is because of GIL(Global Interpreter Lock)
+# But PS: in multi threaded python program, only one thread can execute at a time even if the cpu has multiple cores. It is because of GIL(Global Interpreter Lock) which allows a thread to hold the interpreter, perform its taks, and release later after completion of the task, and the other thread use it in a similar manner. So hence, no concurrency execution of the thread in python.
 
 # Hence I/o bound task are more preferred to be handle by the concept of multi-threading whereas, cpu bound task are preferred to be handled by multi processing.
 
