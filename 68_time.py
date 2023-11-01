04cff6cdf5b331868ddd37c36df1df44b3dc3b81
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-11-01 06:35:28 +0545

    files updated

--- 68_time.py
+++ 68_time.py
@@ -4,3 +4,5 @@ import time
 
 print(time.ctime(0)) #this will give me my epoch time, yeah i have already talked about epoch time in previous session, but generally epoch means that particular time from where couting is started(for python its jan 1, 1970)
 # You may be wondering why there is argument 0? well it is nothing but hwo much seconds later do you want epoch to print times.
+
+print(time.ctime(100000000000000)) #i am saying here, give me the time after 10000000..... seconds 
