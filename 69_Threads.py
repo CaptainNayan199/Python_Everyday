f5369a54706c2bbc5cab615d97ba78b88a2b87cc
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-11-01 10:02:06 +0545

    files updated

--- 69_Threads.py
+++ 69_Threads.py
@@ -1,2 +1,6 @@
 # So what are threads ?
-# In python thread are light weight process
+# In python thread are light weight process, threads are fundamental part of modern programming.
+
+# Thread allows a program to operate more efficiently by handling multiple tasks at the same time.
+
+# In python there is a built in module for thread called as `threading`, using it we can create a new thread, manage them, and also communicate between them.
