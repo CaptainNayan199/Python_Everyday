5322c6735602a3920aff8772505ad9f73b3e65b1
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-11-01 06:33:31 +0545

    files updated

--- 68_time.py
+++ 68_time.py
@@ -2,4 +2,4 @@
 
 import time
 
-print(time.ctime(0))
+print(time.ctime(0)) #this will give me my epoch time, yeah i have already talked about epoch time in previous session, but generally epoch means that particular time from where couting is started(for python its jan 1, 1970)
