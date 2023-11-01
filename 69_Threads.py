45334fb7395b53816c91b2ab28bd0b89d770ad68
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-11-01 10:03:34 +0545

    files updated

--- 69_Threads.py
+++ 69_Threads.py
@@ -2,6 +2,6 @@
 # In python thread are light weight process, threads are fundamental part of modern programming.
 
 # Thread allows a program to operate more efficiently by handling multiple tasks at the same time.
-# Threads share same memory space because of which data sharing among threads is completely a normal things.
+# Threads share same memory space because of which data sharing among threads is completely a normal things. But this freedom can also lead to problem like deadlock and data races(which thread will use a particular data for the first time)
 
 # In python there is a built in module for thread called as `threading`, using it we can create a new thread, manage them, and also communicate between them.
