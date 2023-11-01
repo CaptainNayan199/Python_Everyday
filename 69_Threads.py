b73498ca27612de916b013d92b3a30f1a39dba08
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-11-01 10:06:01 +0545

    files updated

--- 69_Threads.py
+++ 69_Threads.py
@@ -5,3 +5,7 @@
 # Threads share same memory space because of which data sharing among threads is completely a normal things. But this freedom can also lead to problem like deadlock and data races(which thread will use a particular data for the first time)
 
 # In python there is a built in module for thread called as `threading`, using it we can create a new thread, manage them, and also communicate between them.
+
+# But PS: in multi threaded python program, only one thread can execute at a time even if the cpu has multiple cores.
+
+# Hence I/o bound task are more preferred to be handle by the concept of multi-threading whereas, cpu bound task are preferred to be handled by multi processing.
